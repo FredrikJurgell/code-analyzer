7f5fd6111d9ed4e5481d1599ce8d515487d13d39
Fredrik Jurgell <fj222qp@student.lnu.se>
2022-09-19 09:19:48 +0200

    Can now read from folder, from github and count number of characters.

--- src/code-analyzer.js
+++ src/code-analyzer.js
@@ -1,19 +1,73 @@
 import * as fs from 'fs'
+import fetch from 'node-fetch'
 
 /**
  * Analyzes your code.
  */
 export default class CodeAnalyzer {
   /**
-   * Returns the read code as a string.
+   * Returns the read code from the file as a string.
    *
    * @param {string} fileName - The name of the file.
-   * @returns {string} - The code as a string..
+   * @returns {string} - The code as a string.
    */
   fileToRead (fileName) {
-    const readFile = fs.readFileSync(`src/${fileName}`, 'utf-8')
+    try {
+      const readFile = fs.readFileSync(fileName, 'utf-8')
 
-    return readFile
+      return readFile
+    } catch (error) {
+      return 'Invalid file-name.'
+    }
+  }
+
+  /**
+   * Returns the read folders files as a array.
+   *
+   * @param {string} folderName - The name of the file.
+   * @returns {Array} - The code as a array.
+   */
+  folderToRead (folderName) {
+    try {
+      const readFolder = fs.readdirSync(folderName, 'utf-8')
+      const newArray = []
+
+      for (let i = 0; i < readFolder.length; i++) {
+        if (readFolder[i].match(/.js/g)) {
+          newArray.push(readFolder[i])
+        }
+      }
+
+      return newArray
+    } catch (error) {
+      return 'Invalid folder-name.'
+    }
+  }
+
+  /**
+   * Returns the read code from a file from a github-repository as a string.
+   *
+   * @param {string} owner - The account owner of the repository. The name is not case sensitive.
+   * @param {string} repo - The name of the repository. The name is not case sensitive.
+   * @param {string} path - The path.
+   * @returns {string} - The code as a string.
+   */
+  async readFileFromGithub (owner, repo, path) {
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'bearer-token': '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'
+      }
+    })
+
+    const resonseJson = await response.json()
+    const buff = Buffer.from(resonseJson.content, 'base64')
+
+    const str = buff.toString('utf-8')
+
+    return str
   }
 
   /**
@@ -23,7 +77,11 @@ export default class CodeAnalyzer {
    * @returns {number} - The number of lines.
    */
   linesCounter (data) {
-    let numberofLines = 1
+    let numberofLines = 0
+
+    if (!data.length) {
+      return 0
+    }
 
     for (let i = 0; i < data.length; i++) {
       if (data[i] === '\n') { // if new line.
@@ -42,9 +100,15 @@ export default class CodeAnalyzer {
    */
   forLoopsCounter (data) {
     let numberOfForLoops = 0
-    const forLoopsArray = data.match(/for \(/g) // match "for (".
-    for (let i = 0; i < forLoopsArray.length; i++) {
-      numberOfForLoops++
+
+    const forLoopsArray = data.match(/for \(/g) || [] // match "for (".
+
+    if (forLoopsArray.length === 0) {
+      return 0
+    } else {
+      for (let i = 0; i < forLoopsArray.length; i++) {
+        numberOfForLoops++
+      }
     }
 
     return numberOfForLoops
@@ -58,10 +122,15 @@ export default class CodeAnalyzer {
    */
   inlineCommentsCounter (data) {
     let numberOfComments = 0
-    const commentsArray = data.match(/\/\/ /g) // match "// ".
 
-    for (let i = 0; i < commentsArray.length; i++) {
-      numberOfComments++
+    const commentsArray = data.match(/\/\/ /g) || [] // match "// ".
+
+    if (commentsArray.length === 0) {
+      return 0
+    } else {
+      for (let i = 0; i < commentsArray.length; i++) {
+        numberOfComments++
+      }
     }
 
     return numberOfComments
@@ -75,9 +144,15 @@ export default class CodeAnalyzer {
    */
   whileLoopsCounter (data) {
     let numberOfWhileLoops = 0
-    const whileLoopsArray = data.match(/while \(/g) // match "while (".
-    for (let i = 0; i < whileLoopsArray.length; i++) {
-      numberOfWhileLoops++
+
+    const whileLoopsArray = data.match(/while \(/g) || [] // match "while (".
+
+    if (whileLoopsArray.length === 0) {
+      return 0
+    } else {
+      for (let i = 0; i < whileLoopsArray.length; i++) {
+        numberOfWhileLoops++
+      }
     }
 
     return numberOfWhileLoops
@@ -91,11 +166,27 @@ export default class CodeAnalyzer {
    */
   returnsCounter (data) {
     let numberOfReturns = 0
-    const returnsArray = data.match(/return /g) // match "return ".
-    for (let i = 0; i < returnsArray.length; i++) {
-      numberOfReturns++
+
+    const returnsArray = data.match(/return /g) || [] // match "return ".
+
+    if (returnsArray.length === 0) {
+      return 0
+    } else {
+      for (let i = 0; i < returnsArray.length; i++) {
+        numberOfReturns++
+      }
     }
 
     return numberOfReturns
+  }
+
+  /**
+   * Returns the number of characters in the code.
+   *
+   * @param {string} data - The code.
+   * @returns {number} - The number of characters.
+   */
+  charactersCounter (data) {
+    return data.length
   }
 }
