b78f10962a71b0b572052814896ea979b0b0fb02
Fredrik Jurgell <fj222qp@student.lnu.se>
2022-10-12 08:47:35 +0200

    Changed names of methods in example-app

--- example-app/src/example-app.js
+++ example-app/src/example-app.js
@@ -5,41 +5,41 @@ import CodeAnalyzer from '../../src/code-analyzer.js'
  */
 const main = async function () {
   let codeAnalyzer = new CodeAnalyzer('example-app/src/code-example.js')
-  let file = codeAnalyzer.fileToRead()
+  let file = codeAnalyzer.readFile()
 
-  const numberOfLines = codeAnalyzer.linesCounter(file)
+  const numberOfLines = codeAnalyzer.countLines(file)
   console.log('There are ' + numberOfLines + ' lines.')
 
-  const numberOfForLoops = codeAnalyzer.forLoopsCounter(file)
+  const numberOfForLoops = codeAnalyzer.countForLoops(file)
   console.log('There are ' + numberOfForLoops + ' for-loops.')
 
-  const numberOfInlineComments = codeAnalyzer.inlineCommentsCounter(file)
+  const numberOfInlineComments = codeAnalyzer.countInlineComments(file)
   console.log('There are ' + numberOfInlineComments + ' inline-comments.')
 
-  const numberOfWhileLoops = codeAnalyzer.whileLoopsCounter(file)
+  const numberOfWhileLoops = codeAnalyzer.countWhileLoops(file)
   console.log('There are ' + numberOfWhileLoops + ' while-loops.')
 
-  const numberOfreturns = codeAnalyzer.returnsCounter(file)
+  const numberOfreturns = codeAnalyzer.countReturns(file)
   console.log('There are ' + numberOfreturns + ' returns.')
 
-  const numberOfCharacters = codeAnalyzer.charactersCounter(file)
+  const numberOfCharacters = codeAnalyzer.countCharacters(file)
   console.log('There are ' + numberOfCharacters + ' characters.')
 
-  const numberOfVariables = codeAnalyzer.variablesCounter(file)
+  const numberOfVariables = codeAnalyzer.countVariables(file)
   console.log('There are ' + numberOfVariables + ' variables.')
 
-  const numberOfJsdocComments = codeAnalyzer.jsdocCommentsCounter(file)
+  const numberOfJsdocComments = codeAnalyzer.countJsdocComments(file)
   console.log('There are ' + numberOfJsdocComments + ' jsdoc-comments.')
 
-  const folder = codeAnalyzer.folderToRead('example-app/src/my-memory-game')
+  const folder = codeAnalyzer.readFolder('example-app/src/my-memory-game')
   codeAnalyzer = new CodeAnalyzer('example-app/src/my-memory-game/' + folder[0])
-  let fileFromFolder = codeAnalyzer.fileToRead()
+  let fileFromFolder = codeAnalyzer.readFile()
 
-  const numberOfLinesFolder = codeAnalyzer.linesCounter(fileFromFolder)
+  const numberOfLinesFolder = codeAnalyzer.countLines(fileFromFolder)
   console.log('There are ' + numberOfLinesFolder + ' lines.')
 
   const fileGithub = await codeAnalyzer.readFileFromGithub('fredrikjurgell', '1dv610-l0', 'src/server.js')
-  console.log('There are ' + codeAnalyzer.linesCounter(fileGithub) + ' lines in the code from GitHub.')
+  console.log('There are ' + codeAnalyzer.countLines(fileGithub) + ' lines in the code from GitHub.')
 }
 
 main()
